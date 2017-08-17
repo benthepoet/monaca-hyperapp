d56e2f160a490bf8eef96e29a3a28bb12e5b4845
Ben Hanna <benpaulhanna@gmail.com>
2017-08-17 02:10:55 +0000

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,88 @@
+// NPM modules
+const ons = require('onsenui');
+const { app, h } = require('hyperapp');
+
+// Onsen UI Stylesheets
+require('onsenui/css/onsen-css-components.min.css');
+require('onsenui/css/onsenui.css');
+
+app({
+    actions: {
+        popPage: ({ pages }, actions, { ref }) => {
+            return update => {
+                const fn = () => {
+                    return new Promise((resolve) => {
+                        update({
+                            pages: pages.slice(0, pages.length - 1)
+                        });
+                        resolve();
+                    });  
+                };
+                
+                ref._popPage({}, fn);
+            };
+        },
+        pushPage: ({ pages }, actions, { ref, page }) => {
+            console.log(ref);
+            return update => {
+                const fn = () => {
+                    return new Promise((resolve) => {
+                        update({
+                            pages: pages.concat([page])
+                        });
+                        requestAnimationFrame(resolve);
+                    });  
+                };
+                
+                ref._pushPage({}, fn);
+            };
+        }
+    },
+    state: {
+        pages: [Main]
+    },
+    view: (state, { popPage, pushPage }) => {
+        let ref;
+        
+        const setRef = (element) => { ref = element; };
+        
+        const pages = state.pages.map((page) => {
+            return page({ 
+                popPage: () => {
+                    popPage({ ref });
+                },
+                pushPage: (options) => {
+                    pushPage(Object.assign({}, options, { ref }));
+                }
+            });
+        });
+        
+        return <ons-navigator oncreate={setRef} onupdate={setRef}>
+                {pages}
+            </ons-navigator>;
+    }
+});
+
+function Page({ title }, children) {
+    return h('ons-page', {}, [
+        h('ons-toolbar', {}, [
+            h('div', { class: 'center' }, title)
+        ]),
+        h('div', {}, null),
+        h('div', { class: 'content' }, children)
+    ]);
+}
+
+function Main({ pushPage }) {
+    return <Page title="Main Page">
+            <ons-input modifier="underbar" type="text" />
+            <ons-button onclick={() => pushPage({ page: Second })}>Push Page</ons-button>
+        </Page>;
+}
+
+function Second({ popPage }) {
+    return <Page title="Second Page">
+            <p>Second Page</p>
+            <ons-button onclick={popPage}>Pop Page</ons-button>
+        </Page>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { resolve } = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+        output: {
+            path: resolve(__dirname, 'www'),
+            filename: 'bundle.js'
+        },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    plugins: [
+                        ['transform-react-jsx', { pragma: 'h' }]
+                    ],
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?\S*)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/[name].[hash].[ext]'
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: resolve(__dirname, 'www'),
+        disableHostCheck: true
+    }
+};
